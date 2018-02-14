597e7ee630933554a378cbcf7e5e58cbbb456e54
Wenyan Bi <wb1918a@student.american.edu>
2018-02-15 06:56:58 +0800

    Update randomly_shuffle_files_the_same_way.sh

--- randomly_shuffle_files_the_same_way.sh
+++ randomly_shuffle_files_the_same_way.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
+# This code will randomly shuttle all files according to the same rule.
 
+# Written by Wenyan Bi on Jan 30th, 2018.
+
+# random rule; 
 dd if=/dev/random of=myrand count=1024
 
 for x in OpaqueThickCotton*; do
+	# Need to install gshuf
 	# brew install coreutils
 	gshuf --random-source=myrand <"$x" >"$x.tmp"
 	mv "$x.tmp" "$x"
