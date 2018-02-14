7d3cf4fd33172f5408d9a9263df9ac347a59bcd4
Wenyan Bi <wb1918a@student.american.edu>
2018-02-15 06:57:58 +0800

    Update Insert_lines.sh

--- Insert_lines.sh
+++ Insert_lines.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# written by wwenyan bi on Jan 31, 2018
 # This code will insert lines to a file
+# Written by wwenyan bi on Jan 31, 2018
 
 for file in OpaqueThickCotton*; do
   # a\: will insert after the first line 
